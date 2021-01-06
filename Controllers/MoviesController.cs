d3c6c1da474fe74aa9ea1b7f55dc7092021d2720
Florent Biville <florent.biville@neo4j.com>
2021-01-06 17:28:38 +0100

    Push initial version (without CI yet)

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MoviesDotNetCore.Model;
+using MoviesDotNetCore.Repositories;
+
+namespace MoviesDotNetCore.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieRepository _movieRepository;
+
+        public MoviesController(MovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [Route("/movie/{title}")]
+        [HttpGet]
+        public async Task<Movie> GetMovieDetails([FromRoute(Name = "title")] string title)
+        {
+            return await _movieRepository.FindByTitle(title);
+        }
+
+        [Route("/search")]
+        [HttpGet]
+        public async Task<List<Movie>> SearchMovies([FromQuery(Name = "q")] string search)
+        {
+            return await _movieRepository.Search(search);
+        }
+
+        [Route("/graph")]
+        [HttpGet]
+        public async Task<D3Graph> FetchD3Graph([FromQuery(Name = "limit")] int limit)
+        {
+            return await _movieRepository.FetchD3Graph(limit <= 0 ? 50 : limit);
+        }
+    }
+}
