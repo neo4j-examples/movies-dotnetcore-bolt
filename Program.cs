f7c74dc588130e68c1f9ea4989a46b519ae55cd3
Florent Biville <445792+fbiville@users.noreply.github.com>
2021-01-11 13:39:21 +0100

    Make port configurable and add CI with end-to-end tests

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -15,6 +16,7 @@ namespace MoviesDotNetCore
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls($"http://localhost:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}");
                 });
     }
 }
